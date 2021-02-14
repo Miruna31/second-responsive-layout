9d9a45e38f2c35dda4987b59383f1517da19c8f1
Miruna Arcaleanu <mirunaac@gmail.com>
2021-02-14 19:14:23 +0200

    added menu code

---
+++ resources/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const menuIcon =  document.getElementById("menu-icon");
+const mobileMenu = document.getElementById("mobileMenu");
+const menuIconClose =  document.getElementById("menu-icon-close");
+const mainWrapper =document.getElementById("mainWrapper");
+
+
+menuIcon.addEventListener('click', function(){
+    console.log('click');
+    if(mobileMenu.classList.contains('hidden')){
+        mobileMenu.classList.remove('hidden');
+        mainWrapper.style.overflowY = 'hidden';
+    }
+});
+
+menuIconClose.addEventListener('click', function(){
+    console.log('click');
+    mobileMenu.classList.add('hidden');
+});
