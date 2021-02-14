478d609206b47e53445f2639bb2cbb3ce16f373f
Miruna Arcaleanu <mirunaac@gmail.com>
2021-02-14 19:19:26 +0200

    updated code

--- resources/main.js
+++ resources/main.js
@@ -3,7 +3,7 @@
 const menuIcon =  document.getElementById("menu-icon");
 const mobileMenu = document.getElementById("mobileMenu");
 const menuIconClose =  document.getElementById("menu-icon-close");
-const mainWrapper =document.getElementById("mainWrapper");
+const mainWrapper = document.getElementById("mainWrapper");
 
 
 menuIcon.addEventListener('click', function(){
@@ -17,4 +17,5 @@ menuIcon.addEventListener('click', function(){
 menuIconClose.addEventListener('click', function(){
     console.log('click');
     mobileMenu.classList.add('hidden');
+    mainWrapper.style.overflowY = 'scroll';
 });
